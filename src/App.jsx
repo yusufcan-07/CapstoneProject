59c39d7861a4d6d065e99a5234bb8f2cfc84b75c
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2023-10-19 20:31:53 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <div className="text-3xl font-bold underline">Test</div>
+      <div className="text-3xl font-bold underline">Deneme</div>
     </>
   );
 }
