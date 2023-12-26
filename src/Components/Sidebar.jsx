e0b143e87ea76286da87ebf95b2e92f7e37353a2
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2023-12-26 23:26:12 +0300

    login logout button added
    also commented api implementation

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -49,6 +49,14 @@ export default function Sidebar() {
         <CustomLink to={"/news"} icon={<Newspaper size={24} />}>
           KAP News
         </CustomLink>
+        <div className="flex justify-between mt-8 p-8">
+          <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out">
+            Login
+          </button>
+          <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out">
+            Logout
+          </button>
+        </div>
         <div className="sidebar-height" />
         <hr />
         <CustomLink to={"/settings"} icon={<Settings size={24} />}>
